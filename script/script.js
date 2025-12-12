784dc07dd3bf8e337d61fe8ec18ab089bbaca328
Titas-titas <titas.kucinskas@gmail.com>
2025-12-12 12:39:59 +0200

    added text for input error

--- script/script.js
+++ script/script.js
@@ -6,31 +6,54 @@ const companyName = document.querySelector('#companyName');
 const title = document.querySelector('#title');
 const message = document.querySelector('#message');
 const announcements = document.querySelector('#announcements');
+const inputInvalid = document.querySelector('#invalid');
 
 sending.addEventListener("click", (e) => {
     e.preventDefault();
-    const inputs = [name_, companyName, title, message];
-    let countIsEmpty = 0;
-  
+    const inputs = [
+        {input:name_, errorText: document.querySelector('#invalid-name')}, 
+        {input:companyName, errorText: document.querySelector('#invalid-company')}, 
+        {input:title, errorText: document.querySelector('#invalid-title') } , 
+        {input:message, errorText: document.querySelector('#invalid-message') 
 
-    if (!checkTheEmail(email.value)) {
+        }];
+    let countIsEmpty = 0;
+    
+    // žiuri ar email nera tuščia ir taip pat ar tinkamas
+    const invalidEmail = document.querySelector('#invalid-email');
+    const emailError = document.querySelector('#email-error');
+    if(email.value === ""){
         email.style.borderBottom = "1px solid red";
         email.style.color = "red";
+        invalidEmail.classList.remove("hidden");
+        emailError.classList.add("hidden");
+        countIsEmpty++;
+    }
+    else if (!checkTheEmail(email.value)) {
+        email.style.borderBottom = "1px solid red";
+        email.style.color = "red";
+        emailError.classList.remove("hidden");
+        invalidEmail.classList.add("hidden");
         countIsEmpty++;
     } else {
         email.style.borderBottom = "";
         email.style.color = "";
+        invalidEmail.classList.add("hidden");
+        emailError.classList.add("hidden");
     }
 
-    inputs.forEach(input => {
+    // čia žiuri ar netuščia
+    inputs.forEach(({input, errorText}) => {
         if (input.value == "") {
             input.style.borderBottom = "1px solid red";
             input.style.color = "red";
+            errorText.classList.remove("hidden");
             countIsEmpty++;
         }
         else {
             input.style.borderBottom = "";
             input.style.color = "";
+            errorText.classList.add("hidden");
         }
     });
 
@@ -45,12 +68,10 @@ sending.addEventListener("click", (e) => {
 const checkTheEmail = (email) => {
     const usersEmail = email.trim();
     const parts = usersEmail.split("@");
-
     if (parts.length !== 2) return false;
 
     const local = parts[0];
     const domain = parts[1];
-
     if (local.length === 0) return false;
 
     const domainParts = domain.split(".");
